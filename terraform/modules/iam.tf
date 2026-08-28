5873ba38dff2788f6208f5f5cd216581b8890996
sorcerer-ares <vanshsimar14@gmail.com>
2026-08-28 18:56:42 +0000

    more fixes

--- terraform/modules/iam.tf
+++ terraform/modules/iam.tf
@@ -43,9 +43,11 @@ resource "aws_iam_role" "ecs_task_role" {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"] # Standard dummy thumbprint AWS accepts for GitHub
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
 }
-
 # 2. GitHub Actions IAM Role
 resource "aws_iam_role" "github_actions_role" {
   name = "preview-github-actions-role"
