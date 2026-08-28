8d30e4afef542858db44ee9d3fa7f30442b53fde
sorcerer-ares <vanshsimar14@gmail.com>
2026-08-28 19:50:34 +0000

    more more fix

--- terraform/modules/iam.tf
+++ terraform/modules/iam.tf
@@ -59,7 +59,7 @@ resource "aws_iam_role" "github_actions_role" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Effect = "Allow"
         Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+          Federated = "arn:aws:iam::357919579947:oidc-provider/token.actions.githubusercontent.com"
         }
         Condition = {
           "StringEquals" = {
